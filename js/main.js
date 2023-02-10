ccf26f6815a20a4481583afc6d691b72857f942b
AlexZhaoNTIJohanneberg <119052322+AlexZhaoNTIJohanneberg@users.noreply.github.com>
2023-02-10 14:18:22 +0100

    10-2-23
    lade till en simpel grid-layout och fixade "översikts"-knappen. Inget innehåll än

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let overviewButton = document.querySelector(".overview-button");
+overviewButton.addEventListener("click",showoverview);
+
+
+
+
+function showoverview(){ 
+
+    let overview = document.querySelector("div.overview");
+    overview.classList.toggle("showoverview");
+    let movebutton = documentquerySelector("nav.overview-button");
+    overview.classList.toggle("movebutton");
+
+
+};
